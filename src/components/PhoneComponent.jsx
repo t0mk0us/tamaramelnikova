3b8190e5ab0b1541d949599465f434850a4939a9
t0mk0us <tamara9@rogers.com>
2026-01-28 17:26:51 -0500

    Updates

--- src/components/PhoneComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-const PhoneComponent = ({ phone, children }) => {
-  return <a href={`tel:${phone}`} className='linkedin-link'>{children}</a>;
-};
-
-export default PhoneComponent;
